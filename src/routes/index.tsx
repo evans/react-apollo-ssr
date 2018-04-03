e032b5e8164daeaebc6a948fd4055f7507885295
Evans Hauser <evanshauser@gmail.com>
2018-04-03 16:28:32 -0700

    remove state and toggling from client

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,58 +6,41 @@ import gql from 'graphql-tag';
 // import MainPage from './MainPage';
 // import AnotherPage from './AnotherPage';
 
-const ALL_ITEMS_QUERY = gql`
+export const ALL_ITEMS_QUERY = gql`
   query HelloWorld {
     hello
   }
 `;
 
-class MainPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
+const MainPage = () => (
+  <Query query={ALL_ITEMS_QUERY} fetchPolicy="cache-first">
+    {({ data, error, loading, refetch }) => {
+      if (loading) return <div>Loading</div>;
+      if (error) return <div>Error</div>;
+      return (
+        <div>
+          <button
+            onClick={() => {
+              alert('alert');
+            }}
+          >
+            alert
+          </button>
 
-  handleClick(event) {
-    this.setState({ liked: !this.state.liked });
-  }
-  render() {
-    return (
-      <Query query={ALL_ITEMS_QUERY} fetchPolicy="cache-first">
-        {({ data, error, loading, refetch }) => {
-          if (loading) return <div>Loading</div>;
-          if (error) return <div>Error</div>;
-          const text = this.state.liked ? 'like' : "haven't liked";
-          return (
-            <div>
-              <p onClick={this.handleClick}>
-                You {text} this. Click to toggle.
-              </p>
-              <button
-                onClick={() => {
-                  alert('alert');
-                }}
-              >
-                alert
-              </button>
-
-              <button
-                onClick={() => {
-                  console.log('calling refetch');
-                  refetch().then(() => console.log('refetch finished'));
-                }}
-              >
-                refetch
-              </button>
-              <p>{data.hello} </p>
-            </div>
-          );
-        }}
-      </Query>
-    );
-  }
-}
-
+          <button
+            onClick={() => {
+              console.log('calling refetch');
+              refetch().then(() => console.log('refetch finished'));
+            }}
+          >
+            refetch
+          </button>
+          <p>{data.hello} </p>
+        </div>
+      );
+    }}
+  </Query>
+);
 const AnotherPage = () => <div> another </div>;
 
 const routes = [
